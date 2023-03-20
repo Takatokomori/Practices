86e14f3fc1565a4de66ec360ddcf21b9278b1fc8
taka <takachan1050150@gmail.com>
2023-03-20 18:08:37 +0900

    delete request files from the origin/develop

--- src/pages/cssCursorFollow.js
+++ src/pages/cssCursorFollow.js
@@ -1,6 +1,14 @@
 import React from 'react'
+import $ from 'jquery'
 
 export default function CssCursorFollow() {
+  jQuerycode = () =>{
+    var magic = $(".magic");
+    var magicWHalf = $magic.width() / 2;
+    $(document).on("mousemove", function(e) {
+      magic.css({"left": e.pageX - magicWHalf, "top": e.pageY - magicWHalf});
+    });
+  }
   return (
     <div className="scene">
         <div className="magic"></div>
