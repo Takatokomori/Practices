330baf235573a745750ab65d30d69604c4c2403e
taka <takachan1050150@gmail.com>
2023-03-21 08:43:43 +0900

    fixed path

--- src/components/header.js
+++ src/components/header.js
@@ -32,7 +32,7 @@ export default function Header() {
               </div>
               <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
                 <div className="flex flex-shrink-0 items-center">
-                  <a href='/'>
+                  <a href='/practices'>
                     <img
                       className="block h-8 w-auto lg:hidden rotate-180"
                       src={Logo}
