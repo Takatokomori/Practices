56cbe0731086283d98dff7eaa9a6aca825101600
editor-taka <takachan1050150@gmail.com>
2023-03-21 21:43:38 +0900

    reduce css in cssCUrsor

--- src/pages/cssCursorFollow.js
+++ src/pages/cssCursorFollow.js
@@ -1,8 +1,17 @@
-import React from 'react'
+import React, {useState} from 'react'
 import $ from 'jquery'
 import "../styles/cssCursorFollow.css"
 
 export default function CssCursorFollow() {
+
+  const [cursorX, setCursorX ] =  useState();
+  const [cursorY, setCursorY ] =  useState();
+
+  window.addEventListener("mousemove", (e) =>{
+    setCursorX(e.pageX);
+    setCursorY(e.pageY);
+  });
+
   $(document).on("mousemove", function(e) {
     var magic = $(".magic");
     var magicWHalf = magic.width() / 2;
@@ -10,8 +19,19 @@ export default function CssCursorFollow() {
   });
 
   return (
-    <div className="scene">
-        <div className="magic"></div>
-    </div>
+      <>
+        {/* <div className="scene">
+          <div className="magic"></div>
+        </div> */}
+        
+        <div className='sample-one'>
+          <div className='sample-size'></div>
+          <div className='sample-img'></div>
+          <div className='cursor' style={{
+            left: cursorX + "px",
+            top: cursorY + "px",
+          }}></div>
+        </div>
+      </> 
   )
 }
