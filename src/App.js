477bb6ce0cb24b5de6f13c41da2388f34a533e17
editor-taka <takachan1050150@gmail.com>
2023-03-22 16:14:56 +0900

    add motion bar

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Layout from "./components/layout";
 import CssCursorFollow from "./pages/cssCursorFollow";
+import CssMotionBar from "./pages/cssMotionBar";
 import Index from "./pages/index";
 
 function App() {
@@ -11,6 +12,8 @@ function App() {
           <Route path="practices" element={<Index />} ></Route>
           <Route path="practices/cssCursorFollow" 
                  element={<CssCursorFollow />} ></Route>
+          <Route path="practices/cssMotionBar" 
+                 element={<CssMotionBar />} ></Route>
         </Routes>
       </Layout>
     </BrowserRouter>
